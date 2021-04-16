0e1defa8ee5d087b5f6be284d16d06de64fb58aa
N33ds0n <78763485+N33ds0n@users.noreply.github.com>
2021-04-16 05:44:50 +0100

    Add files via upload

---
+++ vcolor.py
@@ -0,0 +1,61 @@
+""" Coded By The VedaLogic Team
+
+    @Nerd
+    @Kcubeterm
+    @Raymond
+    @it's pointless
+    @$ugar
+    @Skylord
+    @Thierry
+    @kc
+    @Jeff
+    @Punisher
+    @Micro Biology
+    @other members
+
+   You Can Modified this Source Code to Your own Taste 
+   You Can Add More Colors.
+
+
+"""
+
+
+
+banner = """
+
+
+.##.....##..######...#######..##........#######..########.
+.##.....##.##....##.##.....##.##.......##.....##.##.....##
+.##.....##.##.......##.....##.##.......##.....##.##.....##
+.##.....##.##.......##.....##.##.......##.....##.########.
+..##...##..##.......##.....##.##.......##.....##.##...##..
+...##.##...##....##.##.....##.##.......##.....##.##....##.
+....###.....######...#######..########..#######..##.....##
+
+
+"""
+
+
+
+class Vcolor:
+
+    black = "\4u001b[30m"
+    red = "\u001b[31m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue =  "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+    reset = "\u001b[0m"
+
+
+
+#this is a simple program you can play with
+
+"""
+
+all you have to do is ...
+
+
+"""
